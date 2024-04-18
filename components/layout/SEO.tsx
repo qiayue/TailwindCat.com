f8e475aabf08d634807b2bdafaa58ff1cacf455b
qiayue <qiayue@gmail.com>
2024-04-18 19:52:07 +0800

    update meta

--- components/layout/SEO.tsx
+++ components/layout/SEO.tsx
@@ -7,171 +7,21 @@ interface Props {
 const SEO = ({ isDark }: Props): JSX.Element => {
   const description =
     'A tailwind multi-tool to convert and visualize your classes properties.'
-  const title = 'TailwindCat'
+  const name = 'TailwindCat'
+  const title = ': Tailwind CSS Width,Height,Flex,Border,Grid,Colors,Components'
   return (
     <Head>
       <>
         <link
           rel='icon'
           type='image/png'
-          sizes='196x196'
-          href='/icons/favicon-196.png'
+          sizes='192x192'
+          href='/icons/android-chrome-192x192.png'
         />
-        <link rel='apple-touch-icon' href='/icons/apple-icon-180.png' />
+        <link rel='apple-touch-icon' href='/icons/apple-touch-icon.png' />
         <meta name='apple-mobile-web-app-capable' content='yes' />
-        <link
-          rel='apple-touch-startup-image'
-          href='/icons/apple-splash-2048-2732.jpg'
-          media='(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
-        />
-        <link
-          rel='apple-touch-startup-image'
-          href='/icons/apple-splash-2732-2048.jpg'
-          media='(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)'
-        />
-        <link
-          rel='apple-touch-startup-image'
-          href='/icons/apple-splash-1668-2388.jpg'
-          media='(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
-        />
-        <link
-          rel='apple-touch-startup-image'
-          href='/icons/apple-splash-2388-1668.jpg'
-          media='(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)'
-        />
-        <link
-          rel='apple-touch-startup-image'
-          href='/icons/apple-splash-1536-2048.jpg'
-          media='(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
-        />
-        <link
-          rel='apple-touch-startup-image'
-          href='/icons/apple-splash-2048-1536.jpg'
-          media='(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)'
-        />
-        <link
-          rel='apple-touch-startup-image'
-          href='/icons/apple-splash-1668-2224.jpg'
-          media='(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
-        />
-        <link
-          rel='apple-touch-startup-image'
-          href='/icons/apple-splash-2224-1668.jpg'
-          media='(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)'
-        />
-        <link
-          rel='apple-touch-startup-image'
-          href='/icons/apple-splash-1620-2160.jpg'
-          media='(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
-        />
-        <link
-          rel='apple-touch-startup-image'
-          href='/icons/apple-splash-2160-1620.jpg'
-          media='(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)'
-        />
-        <link
-          rel='apple-touch-startup-image'
-          href='/icons/apple-splash-1290-2796.jpg'
-          media='(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
-        />
-        <link
-          rel='apple-touch-startup-image'
-          href='/icons/apple-splash-2796-1290.jpg'
-          media='(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)'
-        />
-        <link
-          rel='apple-touch-startup-image'
-          href='/icons/apple-splash-1179-2556.jpg'
-          media='(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
-        />
-        <link
-          rel='apple-touch-startup-image'
-          href='/icons/apple-splash-2556-1179.jpg'
-          media='(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)'
-        />
-        <link
-          rel='apple-touch-startup-image'
-          href='/icons/apple-splash-1284-2778.jpg'
-          media='(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
-        />
-        <link
-          rel='apple-touch-startup-image'
-          href='/icons/apple-splash-2778-1284.jpg'
-          media='(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)'
-        />
-        <link
-          rel='apple-touch-startup-image'
-          href='/icons/apple-splash-1170-2532.jpg'
-          media='(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
-        />
-        <link
-          rel='apple-touch-startup-image'
-          href='/icons/apple-splash-2532-1170.jpg'
-          media='(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)'
-        />
-        <link
-          rel='apple-touch-startup-image'
-          href='/icons/apple-splash-1125-2436.jpg'
-          media='(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
-        />
-        <link
-          rel='apple-touch-startup-image'
-          href='/icons/apple-splash-2436-1125.jpg'
-          media='(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)'
-        />
-        <link
-          rel='apple-touch-startup-image'
-          href='/icons/apple-splash-1242-2688.jpg'
-          media='(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
-        />
-        <link
-          rel='apple-touch-startup-image'
-          href='/icons/apple-splash-2688-1242.jpg'
-          media='(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)'
-        />
-        <link
-          rel='apple-touch-startup-image'
-          href='/icons/apple-splash-828-1792.jpg'
-          media='(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
-        />
-        <link
-          rel='apple-touch-startup-image'
-          href='/icons/apple-splash-1792-828.jpg'
-          media='(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)'
-        />
-        <link
-          rel='apple-touch-startup-image'
-          href='/icons/apple-splash-1242-2208.jpg'
-          media='(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
-        />
-        <link
-          rel='apple-touch-startup-image'
-          href='/icons/apple-splash-2208-1242.jpg'
-          media='(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)'
-        />
-        <link
-          rel='apple-touch-startup-image'
-          href='/icons/apple-splash-750-1334.jpg'
-          media='(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
-        />
-        <link
-          rel='apple-touch-startup-image'
-          href='/icons/apple-splash-1334-750.jpg'
-          media='(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)'
-        />
-        <link
-          rel='apple-touch-startup-image'
-          href='/icons/apple-splash-640-1136.jpg'
-          media='(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
-        />
-        <link
-          rel='apple-touch-startup-image'
-          href='/icons/apple-splash-1136-640.jpg'
-          media='(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)'
-        />
       </>
-      <link rel='canonical' href='https://tailwindhelper.com' />
-      <link rel='manifest' href='/site.webmanifest' />
+      <link rel='canonical' href='https://tailwindcat.com/' />
       <meta
         name='viewport'
         content='initial-scale=1, viewport-fit=cover, user-scalable=no'
@@ -182,26 +32,26 @@ const SEO = ({ isDark }: Props): JSX.Element => {
         content='black-translucent'
       />
 
-      <title>{title}</title>
+      <title>{name}{title}</title>
       <meta name='description' content={description} />
 
       <meta property='og:type' content='website' />
-      <meta property='og:url' content='https://www.tailwindhelper.com' />
-      <meta property='og:title' content={title} />
+      <meta property='og:url' content='https://tailwindcat.com' />
+      <meta property='og:title' content={name} />
       <meta property='og:description' content={description} />
       <meta property='og:image' content='/og-image.png' />
 
       <meta property='twitter:card' content='summary' />
-      <meta property='twitter:url' content='https://www.tailwindhelper.com/' />
-      <meta property='twitter:title' content={title} />
+      <meta property='twitter:url' content='https://tailwindcat.com/' />
+      <meta property='twitter:title' content={name} />
       <meta property='twitter:creator' content='Seb Graf' />
       <meta property='twitter:description' content={description} />
       <meta property='twitter:image' content='/og-image.png' />
 
-      <meta name='application-name' content={title} />
+      <meta name='application-name' content={name} />
       <meta name='apple-mobile-web-app-capable' content='yes' />
       <meta name='apple-mobile-web-app-status-bar-style' content='default' />
-      <meta name='apple-mobile-web-app-title' content={title} />
+      <meta name='apple-mobile-web-app-title' content={name} />
       <meta name='mobile-web-app-capable' content='yes' />
       <meta name='format-detection' content='telephone=no' />
     </Head>
